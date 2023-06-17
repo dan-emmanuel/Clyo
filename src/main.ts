38d2545483f30bd4d8c239ac2d1ac6e5bbb7104e
Dan-Emmanuel <d.e.azoulay@gmail.com>
2023-06-17 22:39:32 +0300

    project init

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './modules/app/app.module';
+
+declare const module: any;
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, { logger: console });
+  await app.listen(3000);
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
+}
+bootstrap();
